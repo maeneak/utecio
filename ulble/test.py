42f56b6e5b53f6104baf9157d39f96949a60dd58
maeneak <dev@criticalmass.com.au>
2023-09-18 22:28:00 +1000

    Move key logic and add SN update

--- ulble/test.py
+++ ulble/test.py
@@ -5,7 +5,30 @@ from api import api_get_devices
 
 EMAIL = "" # Your U-tec username/email
 PASSWORD = "" # Your U-tec password
+LOCK_PWD = "" # Lock UID (int)
+LOCK_UID = "" # Lock Password (int)
+DEVICE_ADDRESS = "" # Lock Mac Address (UUID on MacOS)
 
+async def main():
+    #await test_api()
+    await test_lib()
+    return
+
+async def test_lib():
+    device = UL1BT("Front", LOCK_UID, LOCK_PWD, DEVICE_ADDRESS)
+    await device.update()
+    return
+
+async def test_api():
+    devices = await api_get_devices(EMAIL, PASSWORD)
+    await devices[0].update()
+    return
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+    
 async def main():
     devices = await api_get_devices(EMAIL, PASSWORD)
     await devices[0].unlock()
