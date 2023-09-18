7b9cf59fb99d6d5e74408f0f6f0cb81d52f244bb
maeneak <dev@criticalmass.com.au>
2023-09-18 22:31:06 +1000

    .

--- ulble/test.py
+++ ulble/test.py
@@ -28,12 +28,3 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.close()
-    
-async def main():
-    devices = await api_get_devices(EMAIL, PASSWORD)
-    await devices[0].unlock()
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
