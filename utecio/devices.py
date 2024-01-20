ac280d119af587fd1424f8fc4fbf091d76f92173
maeneak <20371477+maeneak@users.noreply.github.com>
2024-01-20 18:05:33 +1000

    .

--- utecio/devices.py
+++ utecio/devices.py
@@ -2,51 +2,51 @@ from .enums import ULDeviceModel
 
 
 class BLEDeviceCapability:
-    lock: bool
-    door: bool
-    keypad: bool
-    fingprinter: bool
-    doublefp: bool
-    bluetooth: bool
-    rfid: bool
-    rfid_once: bool
-    rfid_twice: bool
-    autobolt: bool
-    autolock: bool
-    autounlock: bool
-    direction: bool
-    update_ota: bool
-    update_oad: bool
-    update_wifi: bool
-    alerts: bool
-    mutemode: bool
-    passage: bool
-    lockout: bool
-    manual: bool
-    shakeopen: bool
-    moreadmin: bool
-    morepwd: bool
-    timelimit: bool
-    morelanguage: bool
-    needregristerpwd: bool
-    locklocal: bool
-    havesn: bool
-    clone: bool
-    customuserid: bool
-    bt264: bool = True
-    keepalive: bool
-    passageautolock: bool
-    doorsensor: bool
-    zwave: bool
-    needreadmodel: bool
-    needsycbuser: bool
-    bt_close: bool
-    singlelatchboltmortic: bool
-    smartphone_nfc: bool
-    update_2642: bool
-    isautodirection: bool
-    ishomekit: bool
-    isyeeuu: bool
+    lock: bool = False
+    door: bool = False
+    keypad: bool = False
+    fingprinter: bool = False
+    doublefp: bool = False
+    bluetooth: bool = False
+    rfid: bool = False
+    rfid_once: bool = False
+    rfid_twice: bool = False
+    autobolt: bool = False
+    autolock: bool = False
+    autounlock: bool = False
+    direction: bool = False
+    update_ota: bool = False
+    update_oad: bool = False
+    update_wifi: bool = False
+    alerts: bool = False
+    mutemode: bool = False
+    passage: bool = False
+    lockout: bool = False
+    manual: bool = False
+    shakeopen: bool = False
+    moreadmin: bool = False
+    morepwd: bool = False
+    timelimit: bool = False
+    morelanguage: bool = False
+    needregristerpwd: bool = False
+    locklocal: bool = False
+    havesn: bool = False
+    clone: bool = False
+    customuserid: bool = False
+    bt264: bool = False
+    keepalive: bool = False
+    passageautolock: bool = False
+    doorsensor: bool = False
+    zwave: bool = False
+    needreadmodel: bool = False
+    needsycbuser: bool = False
+    bt_close: bool = False
+    singlelatchboltmortic: bool = False
+    smartphone_nfc: bool = False
+    update_2642: bool = False
+    isautodirection: bool = False
+    ishomekit: bool = False
+    isyeeuu: bool = False
     secondsarray = []
     mtimearray = []
 
