bdb6d6bb565196b8c393d61633dc029db3777389
maeneak <20371477+maeneak@users.noreply.github.com>
2024-01-20 08:24:39 +0000

    capabilities

--- utecio/devices.py
+++ utecio/devices.py
@@ -2,139 +2,146 @@ from .enums import ULDeviceModel
 
 
 class BLEDeviceCapability:
-    lock: bool = False
-    door: bool = False
-    keypad: bool = False
-    fingprinter: bool = False
-    doublefp: bool = False
-    bluetooth: bool = False
-    rfid: bool = False
-    rfid_once: bool = False
-    rfid_twice: bool = False
-    autobolt: bool = False
-    autolock: bool = False
-    autounlock: bool = False
-    direction: bool = False
-    update_ota: bool = False
-    update_oad: bool = False
-    update_wifi: bool = False
-    alerts: bool = False
-    mutemode: bool = False
-    passage: bool = False
-    lockout: bool = False
-    manual: bool = False
-    shakeopen: bool = False
-    moreadmin: bool = False
-    morepwd: bool = False
-    timelimit: bool = False
-    morelanguage: bool = False
-    needregristerpwd: bool = False
-    locklocal: bool = False
-    havesn: bool = False
-    clone: bool = False
-    customuserid: bool = False
-    bt264: bool = False
-    keepalive: bool = False
-    passageautolock: bool = False
-    doorsensor: bool = False
-    zwave: bool = False
-    needreadmodel: bool = False
-    needsycbuser: bool = False
-    bt_close: bool = False
-    singlelatchboltmortic: bool = False
-    smartphone_nfc: bool = False
-    update_2642: bool = False
-    isautodirection: bool = False
-    ishomekit: bool = False
-    isyeeuu: bool = False
-    secondsarray = []
-    mtimearray = []
+    def __init__(self) -> None:
+        pass
+
+        self.lock: bool = False
+        self.door: bool = False
+        self.keypad: bool = False
+        self.fingprinter: bool = False
+        self.doublefp: bool = False
+        self.bluetooth: bool = False
+        self.rfid: bool = False
+        self.rfid_once: bool = False
+        self.rfid_twice: bool = False
+        self.autobolt: bool = False
+        self.autolock: bool = False
+        self.autounlock: bool = False
+        self.direction: bool = False
+        self.update_ota: bool = False
+        self.update_oad: bool = False
+        self.update_wifi: bool = False
+        self.alerts: bool = False
+        self.mutemode: bool = False
+        self.passage: bool = False
+        self.lockout: bool = False
+        self.manual: bool = False
+        self.shakeopen: bool = False
+        self.moreadmin: bool = False
+        self.morepwd: bool = False
+        self.timelimit: bool = False
+        self.morelanguage: bool = False
+        self.needregristerpwd: bool = False
+        self.locklocal: bool = False
+        self.havesn: bool = False
+        self.clone: bool = False
+        self.customuserid: bool = False
+        self.bt264: bool = False
+        self.keepalive: bool = False
+        self.passageautolock: bool = False
+        self.doorsensor: bool = False
+        self.zwave: bool = False
+        self.needreadmodel: bool = False
+        self.needsycbuser: bool = False
+        self.bt_close: bool = False
+        self.singlelatchboltmortic: bool = False
+        self.smartphone_nfc: bool = False
+        self.update_2642: bool = False
+        self.isautodirection: bool = False
+        self.ishomekit: bool = False
+        self.isyeeuu: bool = False
+        self.secondsarray = []
+        self.mtimearray = []
 
 
 class Latch5F(BLEDeviceCapability):
     def __init__(self) -> None:
-        super().__init__()
-    bluetooth = True
-    autolock = True
-    update_wifi = True
-    alerts = True
-    mutemode = True
-    doublefp = True
-    keypad = True
-    fingprinter = True
-    needregristerpwd = True
-    havesn = True
-    moreadmin = True
-    timelimit = True
-    passage = True
-    lockout = True
-    bt264 = True
-    keepalive = True
-    passageautolock = True
-    singlelatchboltmortic = True
-    smartphone_nfc = True
-    bt_close = True
+        self.super().__init__()
+
+        self.bluetooth = True
+        self.autolock = True
+        self.update_wifi = True
+        self.alerts = True
+        self.mutemode = True
+        self.doublefp = True
+        self.keypad = True
+        self.fingprinter = True
+        self.needregristerpwd = True
+        self.havesn = True
+        self.moreadmin = True
+        self.timelimit = True
+        self.passage = True
+        self.lockout = True
+        self.bt264 = True
+        self.keepalive = True
+        self.passageautolock = True
+        self.singlelatchboltmortic = True
+        self.smartphone_nfc = True
+        self.bt_close = True
 
 
 class Latch5NFC(BLEDeviceCapability):
     def __init__(self) -> None:
         super().__init__()
-    bluetooth = True
-    autolock = True
-    update_wifi = True
-    alerts = True
-    mutemode = True
-    rfid = True
-    rfid_twice = True
-    keypad = True
-    needregristerpwd = True
-    havesn = True
-    moreadmin = True
-    timelimit = True
-    passage = True
-    lockout = True
-    bt264 = True
-    keepalive = True
-    passageautolock = True
-    singlelatchboltmortic = True
-    smartphone_nfc = True
-    bt_close = True
+
+        self.bluetooth = True
+        self.autolock = True
+        self.update_wifi = True
+        self.alerts = True
+        self.mutemode = True
+        self.rfid = True
+        self.rfid_twice = True
+        self.keypad = True
+        self.needregristerpwd = True
+        self.havesn = True
+        self.moreadmin = True
+        self.timelimit = True
+        self.passage = True
+        self.lockout = True
+        self.bt264 = True
+        self.keepalive = True
+        self.passageautolock = True
+        self.singlelatchboltmortic = True
+        self.smartphone_nfc = True
+        self.bt_close = True
 
 
 class UL1BT(BLEDeviceCapability):
     def __init__(self) -> None:
         super().__init__()
-    bluetooth = True
-    rfid = True
-    rfid_twice = True
-    fingprinter = True
-    autobolt = True
-    update_ota = True
-    update_oad = True
-    alerts = True
-    shakeopen = True
-    mutemode = True
-    passage = True
-    lockout = True
-    havesn = True
-    direction = True
-    keepalive = True
-    singlelatchboltmortic = True
+
+        self.bluetooth = True
+        self.rfid = True
+        self.rfid_twice = True
+        self.fingprinter = True
+        self.autobolt = True
+        self.update_ota = True
+        self.update_oad = True
+        self.alerts = True
+        self.shakeopen = True
+        self.mutemode = True
+        self.passage = True
+        self.lockout = True
+        self.havesn = True
+        self.direction = True
+        self.keepalive = True
+        self.singlelatchboltmortic = True
 
 
 class GenericLock(BLEDeviceCapability):
     def __init__(self) -> None:
         super().__init__()
-    bluetooth = True
-    autolock = True
-    mutemode = True
-    havesn = True
-    timelimit = True
-    passage = True
-    lockout = True
-    bt264 = True
-    keepalive = True
-    bt_close = True
+
+        self.bluetooth = True
+        self.autolock = True
+        self.mutemode = True
+        self.havesn = True
+        self.timelimit = True
+        self.passage = True
+        self.lockout = True
+        self.bt264 = True
+        self.keepalive = True
+        self.bt_close = True
 
 
 defined_capabilities: dict[str, BLEDeviceCapability] = {
