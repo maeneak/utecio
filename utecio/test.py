19918d68dfe3a2d9f19ced3fafb6d2e8466107bd
maeneak <20371477+maeneak@users.noreply.github.com>
2024-02-04 21:11:51 +1000

    Update example

--- utecio/test.py
+++ utecio/test.py
@@ -1,41 +1,46 @@
 import asyncio
 
 from bleak import BleakScanner
-
-from .ble import UtecBleLock
-from .api import UtecClient
+from bleak.backends.device import BLEDevice
+from .ble.lock import UtecBleLock
+from .api import UtecClient, logger as liblogger
 
 EMAIL = "your@email.com" # Your Utec app username/email
 PASSWORD = "your_password" # Your Utec App Password
 
-async def update_lock(lockname: str):
+bleak_scanner = BleakScanner()
+
+async def async_bledevice_callback(address:str) -> BLEDevice:
+    # we need to provide a valid BLEDevice for the mac address when asked, or return None
+    # in Home Assistant we should call 'bluetooth.async_ble_device_from_address' to return the BLEDevice.
+    if bleak_scanner:
+        device = await bleak_scanner.find_device_by_address(address)
+        return device
+    return None
+
+async def unlock_lock(lockname: str):
+    # enable debug output
+    liblogger.setLevel(10)
     # connect to webapi and retrieve locks
     client = UtecClient(EMAIL, PASSWORD)
     ble_devices = await client.get_ble_devices()
 
     # select a lock based on a known property (e.g. name)
     l5: UtecBleLock = list(filter(lambda lock: lock.name == lockname, ble_devices))[0]
-
-    # scan for the device (will use a short timmeout because lock is likely asleep)
-    async with BleakScanner() as scanner:
-        device = await scanner.find_device_by_address(l5.mac_uuid, 3)
-
-        # if not discoverable check if it has a wakeup receiver and wake it up and/else try again with long timeout
-        if not device:
-            if l5.wurx_uuid:
-                device = await scanner.find_device_by_address(l5.wurx_uuid, 20)
-                if not device:
-                    raise ConnectionAbortedError("Wakeup controller not available")
-                await l5.async_wakeup_device(device)
-            device = await scanner.find_device_by_address(l5.mac_uuid)
-
-    # if device discoverable request a status update else abort 
-    if device:
-        await l5.async_update_status(device)
+    # register a callback to provide bleak BLEDevice objects
+    l5.async_bledevice_callback = async_bledevice_callback
+    try:
+        # start the scanner for the BLEDevice callback
+        await bleak_scanner.start()
+        # unlock the lock and retrieve a status update
+        await l5.async_unlock(update=True)
+    except Exception as e:
+        print("Unlock failed.", e)
     else:
-        raise ConnectionAbortedError("Device not available")
+        print("Unlock successfull.")
+    finally:
+        # cleanup
+        await bleak_scanner.stop()
 
-    return
-
-asyncio.run(update_lock("Office Door"))
+asyncio.run(unlock_lock("Office Door"))
     
